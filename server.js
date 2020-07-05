3d0799d7d23570f95459ebfce326f9cc261b0e98
Allen Liu <allen6qi@gmail.com>
2020-07-05 12:30:42 +0800

    adjust cors policy

--- server.js
+++ server.js
@@ -21,18 +21,15 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  })
-);
+app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   })
+// );
 
 app.use(
   session({
