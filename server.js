c28c0df6c3c1436f6b8eb798b674cea8947675b6
Allen Liu <allen6qi@gmail.com>
2020-07-05 23:11:08 +0800

    added express sslify

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+var enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -17,6 +18,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 // app.use(
